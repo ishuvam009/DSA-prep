149aae0b1265db0633e41fe8dca88924ce606dc2
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-10-09 23:29:57 +0530

    Commit

--- Basics/Recursion/two.cpp
+++ Basics/Recursion/two.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 using namespace std;
 
+/* Recursion,
+Base Case,
+Stack Overflow,
+Recursion Tree */
+
+int count = 0;
+
+void printt(){
+
+	if(count == 6){
+	return;
+	}else{
+	cout << "Shuavm" << endl;
+	count ++;
+	printt();
+ }
+}
+
 int main(){
-	int n;
-	cin >> n;
-	cout << n;
+
+	printt();
+
+	return 0;	
 }
