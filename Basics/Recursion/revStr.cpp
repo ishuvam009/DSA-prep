17bd7e24e56b41a5c29a6323ee52964e8d21f81f
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-10-17 22:31:51 +0530

    Commit

---
+++ Basics/Recursion/revStr.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int arr[5];
+	arr[1,2,3,4,5];
+	cout << arr[2];
+	return 0;
+}
---
+++ Basics/Recursion/str.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void
+
+int main(){
+
+	string n;
+	cin >> n;
+	cout << n << endl;
+	if(n==n){
+		cout << "Its a pallindrome.";
+	}
+
+	return 0;
+}
