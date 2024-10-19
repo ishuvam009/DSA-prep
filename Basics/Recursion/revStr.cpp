323931a4a8ac0d0e70c7084b04b3a7cf2688b155
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-10-19 20:13:56 +0530

    Commit

--- Basics/Recursion/revStr.cpp
+++ Basics/Recursion/revStr.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-bool func(int n,int i, string arr[]){
+bool func(int n,int i, const vector<string>& arr){
 	if(i>=n/2) return true;
 	if(arr[i] != arr[n-i-1]) return false;
 	return func(n,i+1,arr);
@@ -10,7 +11,7 @@ bool func(int n,int i, string arr[]){
 int main(){
 	int n;
 	cin >> n;
-	string arr[n];
+	vector<string> arr(n);
 	for(int i=0;i<n;i++) cin >> arr[i];
 	cout << func(n,0,arr);
 	return 0;
