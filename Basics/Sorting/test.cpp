df694be54d17b64602d8f780685f64b8969f7095
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2024-10-23 21:53:54 +0530

    Commit

--- Basics/Sorting/test.cpp
+++ Basics/Sorting/test.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 
-void swaps(int arr, int n){
-	
+void swaps(int arr[], int n){
 	for(int i=0;i<n-1;i++){
-		for(){
-			
+		if(arr[i]>arr[i+1]){
+			swap(arr[i],arr[i+1]);
+			i=0;
 		}
 	}
 }
@@ -16,5 +16,6 @@ int main(){
 	int arr[n];
 	for(int i=0;i<n;i++) cin >> arr[i];
 	swaps(arr,n);
+	for(int i=0;i<n;i++) cout << arr[i] << " ";
 	return 0;
 }
