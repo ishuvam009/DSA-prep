9e1eac2995442ea8e0941784035909b5a026f406
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-01-22 23:51:39 +0530

    Commit

---
+++ Basics/STL/Iterator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+    //calling a value inside the container or vector.
+
+    vector<int> v = {23,100,90,7,6};
+
+    cout << v[1];
+
+    return 0;
+}
