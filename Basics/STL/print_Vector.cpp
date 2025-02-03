a63825c25ab5561e42c34290affa059d082e5179
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-02-03 22:11:24 +0530

    Comit

--- Basics/STL/print_Vector.cpp
+++ Basics/STL/print_Vector.cpp
@@ -5,19 +5,35 @@ using namespace std;
 
 int main(){
 
-    vector<int> t1 = {34,80,30,81,100,982,34,70,11};
+    vector<int> t1 = {34,80,30,81};
 
     vector<string> t2={"GPTo1","DEEPSHARKr1","GEMINI","CLAUDE","OLLAM"};
 
     //printing the vector using end() function.
     vector<int>::iterator p1 = t1.end();
     p1--;
-    cout << *(p1);
+    cout << *(p1) << endl;
 
     //using for loop.
     for(vector<int>::iterator it = t1.begin();it!=t1.end();it++){
         cout << *(it) << " ";
     }
+
+    //Insert element insede the vector
+
+    cout << "\n" << "Insert an element" << endl;
+
+    t1.insert(t1.begin(),400); // This will insert 400 at the begning.
+
+    t1.insert(t1.begin()+3,1); //this will insert 1 at 3rd position.
+
+    t1.insert(t1.begin()+6,{10,20,30,100}); // Multiple insertion at once.
+
+    for(auto it = t1.begin();it!=t1.end();it++){
+        cout << *(it) << " ";
+    }
+
+    //Delete an element.
 
     return 0;
 }
