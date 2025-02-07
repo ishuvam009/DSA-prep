e87672df51a1a42475bbc028bd248ed582ea0bf8
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-02-07 23:37:03 +0530

    Commit

--- Basics/STL/List.cpp
+++ Basics/STL/List.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main(){
 
-    list<int> ls;
+    list<int> ls = {20,90,70,60,30,40};
+
+    ls.push_back(2);
+    
+    ls.emplace_back(4);
+
+    ls.emplace_front(1);
+
+    list<int>::iterator lt = ls.begin();
+    advance(lt,1);
+    
+    for(auto pp=ls.begin();pp!=ls.end();pp++){
+        cout << *pp << " ";
+    }
 
     return 0;
 }
