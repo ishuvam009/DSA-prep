8e5032ed3370e76dcb6d4ad6178aaa07ce34cb2b
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-02-12 00:14:07 +0530

    Commit

--- Basics/Class_Objects/main.cpp
+++ Basics/Class_Objects/main.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 
+class Car{
+    public:
+
+        string Model;
+        string Manufacturer;
+        int price;
+
+        void print(){
+            cout << "The " << Manufacturer << Model << " is available at a price of " << price << " rupees.";
+        }
+};
+
 int main(){
 
     //initialization
@@ -8,5 +20,13 @@ int main(){
     int a = 30;  //copy initialization
     int b(370);  //direct initilization
     int c{40}; //direct list initilization
+
+    Car myCar; //creating a object of the Car class.
+    myCar.Manufacturer = "BMW";
+    myCar.Model = "M5";
+    myCar.price = 20000000;
+
+    myCar.print();
+
     return 0;
 }
