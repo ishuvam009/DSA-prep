0380f6ddd668517150e40b42e10f4aea4a5d0ddc
ishuvam009 <120933772+ishuvam009@users.noreply.github.com>
2025-02-11 23:33:01 +0530

    Commit

---
+++ Basics/Class_Objects/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //initialization
+
+    int a = 30;  //copy initialization
+    int b(370);  //direct initilization
+    int c{40}; //direct list initilization
+    return 0;
+}
